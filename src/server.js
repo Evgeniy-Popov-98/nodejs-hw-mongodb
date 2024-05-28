d4145d952ab57a05ad1db4428b52c84c9501bf5b
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-28 19:31:04 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -36,6 +36,13 @@ export const setupServer = () => {
     if (mongoose.isValidObjectId(contactId)) {
       const contact = await getContactById(contactId);
 
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: `Contact with id ${contactId} not found!`,
+        });
+      }
+
       res.json({
         status: 200,
         message: `Successfully found contact with id ${contactId}!`,
@@ -44,7 +51,7 @@ export const setupServer = () => {
     } else {
       return res.status(404).json({
         status: 404,
-        message: `Contact with id ${contactId} not found!`,
+        message: `Contact with id ${contactId} is not valid `,
       });
     }
   });
