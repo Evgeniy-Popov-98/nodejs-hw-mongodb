fd93cb1dc6a9114aaa99d8ae3617f6257b5fbdfb
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-31 19:58:02 +0300

    new_errorMiddelware

--- src/middleware/errorHandlerMiddleware.js
+++ src/middleware/errorHandlerMiddleware.js
@@ -1,4 +1,24 @@
+import { isHttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
+
 export const errorHandlerMiddleware = (error, req, res, next) => {
+  if (isHttpError(error)) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+
+  if (error instanceof MongooseError) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Mongoose error',
+      data: {
+        message: error.message,
+      },
+    });
+  }
+
   res.status(500).json({
     message: 'Something went wrong',
     error: error.message,
--- src/servies/contacts.js
+++ src/servies/contacts.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { Contact } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
@@ -8,3 +9,26 @@ export const getContactById = async (id) => {
   const data = await Contact.findById(id);
   return data;
 };
+
+export const createContact = async (payload) => {
+  const contact = await Contact.create(payload);
+  return contact;
+};
+
+export const upsertsContact = async (id, payload, options = {}) => {
+  const result = await Contact.findByIdAndUpdate(id, payload, {
+    new: true,
+    includesResultMetadata: true,
+    ...options,
+  });
+
+  if (!result || !result.value) {
+    throw createHttpError(404, 'Contact not foud');
+  }
+
+  return {
+    contact: result.value,
+    isNew: !result?.lastErrorObject?.updatedExisting,
+  };
+};
+// export const
