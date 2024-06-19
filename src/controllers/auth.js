d3e5deb58d5e8f68d51c343b9c838bb59384f112
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-06-19 20:19:50 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -22,7 +22,7 @@ export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
   res.json({
-    status: 201,
+    status: 200,
     message: 'Successfully registered a user!',
     data: { name: user.name, email: user.email },
   });
