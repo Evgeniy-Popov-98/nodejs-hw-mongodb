9cdc68c779a4af6e53178733cbae843f49154d92
Evgeniy <evgeniy6183@gmail.com>
2024-06-28 11:42:01 +0300

    add

--- src/utils/googleOAuthClient.js
+++ src/utils/googleOAuthClient.js
@@ -3,14 +3,15 @@ import { readFile } from 'node:fs/promises';
 import { OAuth2Client } from 'google-auth-library';
 import { env } from './env.js';
 import createHttpError from 'http-errors';
+import { ENV_VARS } from '../constants/constants.js';
 
 const PATH_JSON = path.json(process.cwd(), 'google-oauth.json');
 
 const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
-  clientId: env('GOOGLE_AUTH_CLIENT_ID'),
-  clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
+  clientId: env(ENV_VARS.GOOGLE_AUTH_CLIENT_ID),
+  clientSecret: env(ENV_VARS.GOOGLE_AUTH_CLIENT_SECRET),
   redirectUri: oauthConfig.web.redirect_uris[0],
 });
 export const generateAuthUrl = () => {
@@ -24,12 +25,12 @@ export const generateAuthUrl = () => {
 };
 
 export const validateCode = async (code) => {
-  const respons = googleOAuthClient.getToken(code);
+  const respons = await googleOAuthClient.getToken(code);
 
   if (!respons.tokens.id_token) throw createHttpError(401, 'Unauthorized');
 
-  const ticket = googleOAuthClient.verifyIdToken({
-    idToken: (await respons).tokens.id_token,
+  const ticket = await googleOAuthClient.verifyIdToken({
+    idToken: respons.tokens.id_token,
   });
 
   return ticket;
