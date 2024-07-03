13ae218826997a0de245c127b1f130cfa3321d2a
Evgeniy <evgeniy6183@gmail.com>
2024-07-03 09:43:29 +0300

    fix

--- src/validation/createContactSchema.js
+++ src/validation/createContactSchema.js
@@ -9,7 +9,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().required().messages({
     'any.required': '{{#label}} Is Required!',
   }),
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().messages({
     'any.required': '{{#label}} Is Required!',
   }),
   isFavourite: Joi.boolean(),
